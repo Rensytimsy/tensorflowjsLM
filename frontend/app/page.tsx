393b1d43747775a46237c46a7a740c1da586ba62
Rensytims <rensytimsy03@gmail.com>
2025-07-06 23:31:41 +0300

    updated upload form

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,64 +1,92 @@
 "use client"
 
-import Image from "next/image";
 import {useState, useRef} from "react"
+import axios from "axios"
+
+//This is the images interface
+interface UploadedImage {
+  id: string,
+  img: string
+}
 
 export default function Home() {
-  const inputDomElement = useRef<HTMLInputElement>(null);
-  const [image, setImages] = useState<{ img: string }[]>([]);
 
-  const processUploadedFile = () => {
-    if (!inputDomElement.current?.files) return;
+  const uploadElement = useRef<HTMLInputElement>(null);
+  const [uploadedImages, setUploadedImages] = useState<UploadedImage[]>([]);
+  const [removeImage, setRemoveImage] = useState<boolean>(false);
 
-    const uploadedFiles = Array.from(inputDomElement.current.files);
+  const handleFileUpload = () => {
+    if(!uploadElement.current?.files) return;
+    const uploadedFileImages = Array.from(uploadElement.current?.files);
 
-    const imagePromises = uploadedFiles.map((img) => {
-      return new Promise<{ img: string }>((resolve) => {
-        const imagePreview = new FileReader();
-        imagePreview.readAsDataURL(img);
-        imagePreview.onload = () => {
-          if (typeof imagePreview.result === 'string') {
-            resolve({ img: imagePreview.result });
+    const imagesPromise = uploadedFileImages.map((file) => {
+      return new Promise<UploadedImage>((resolve) => {
+        const filePreview = new FileReader();
+        filePreview.readAsDataURL(file);
+        filePreview.onload = () => {
+          if(typeof filePreview.result === "string"){
+            resolve({
+              id: `${Date.now()}-${Math.random().toString(20).substring(3, 18)}`,
+              img: filePreview.result
+            });
           }
-        };
+        }
       });
     });
 
-    Promise.all(imagePromises).then((uploadedImages) => {
-      setImages((prevImg) => [...prevImg, ...uploadedImages]);
+    Promise.all(imagesPromise).then((newImage) => {
+      setUploadedImages((prevImg) => [...prevImg, ...newImage]);
     });
-  };
+  }
 
-  console.log(image)
+  //const handle file upload to the backend
+  const handleCheckFile = async(e: React.MouseEvent<HTMLButtonElement>) : Promise<void> => {
+    e.preventDefault();
+    const formdata = new FormData();
+    uploadedImages.forEach((image) => {
+      formdata.append("image", image.img);
+    });
+    console.log(formdata.getAll("image"));
+    const response = await axios.post("")
+  }
 
   return (
     <div className="flex flex-row justify-center align-center">
-      <div className="mt-[5%] flex flex-col space-y-4">
-        <form
-          action="POST"
-          encType="multipart/form-data"
-          className="cursor-pointer"
-        >
-          <input
-            type="file"
-            multiple
-            onChange={processUploadedFile}
-            ref={inputDomElement}
-          />
-          <button className="border rounded-md p-2">Detect Image</button>
-        </form>
-        <div className="mt-4 flex flex-row space-x-2">
-          {image.map((imgObj, index) => (
-            <img
-              key={index}
-              src={imgObj.img}
-              alt={`uploaded-preview-${index}`}
-              className="w-40 h-40 object-cover mb-4"
-            />
+      <div className="flex flex-col mt-[10%]">
+        <div className="flex flex-row space-x-4">
+          {uploadedImages.map((img) => (
+            <div key={img.id}>
+              <img 
+              src={img.img} 
+              alt="an uploaded image" 
+              className="max-w-[200px] max-h-[200px]"
+              />
+            </div>
           ))}
         </div>
+        <div
+        className="flex flex-row space-x-4 p-2"
+        >
+          <form 
+            method="POST"
+            encType="multipart/form-data"
+            onSubmit={handleCheckFile}
+          >
+            <input 
+            type="file"
+            ref={uploadElement}
+            multiple
+            onChange={handleFileUpload}
+            />
+            <button
+            className="p-1 bg-blue-300 cursor-pointer"
+            type="submit"
+            >
+              Detect Image
+            </button>
+          </form>
+        </div>
       </div>
-
     </div>
-  );
+  )
 }
