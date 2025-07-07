d3ce03e9ad77e6d07a137d3b6c15d6f3566062b4
Rensytims <rensytimsy03@gmail.com>
2025-07-07 22:46:42 +0300

    updated frontend ui

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,20 +6,22 @@ import axios from "axios"
 //This is the images interface
 interface UploadedImage {
   id: string,
-  img: string
+  img: string,
+  file: File
 }
 
 export default function Home() {
 
   const uploadElement = useRef<HTMLInputElement>(null);
   const [uploadedImages, setUploadedImages] = useState<UploadedImage[]>([]);
-  const [removeImage, setRemoveImage] = useState<boolean>(false);
+  const [result, setResult] = useState<string[]>([]);
 
   const handleFileUpload = () => {
     if(!uploadElement.current?.files) return;
     const uploadedFileImages = Array.from(uploadElement.current?.files);
 
     const imagesPromise = uploadedFileImages.map((file) => {
+      console.log(file);
       return new Promise<UploadedImage>((resolve) => {
         const filePreview = new FileReader();
         filePreview.readAsDataURL(file);
@@ -27,7 +29,8 @@ export default function Home() {
           if(typeof filePreview.result === "string"){
             resolve({
               id: `${Date.now()}-${Math.random().toString(20).substring(3, 18)}`,
-              img: filePreview.result
+              img: filePreview.result,
+              file: file
             });
           }
         }
@@ -39,30 +42,53 @@ export default function Home() {
     });
   }
 
+  const removeSelectedImg = (imageId: string) => {
+    setUploadedImages((prevImageId) => uploadedImages.filter((img) => img.id !== imageId));
+  }
+
+
   //const handle file upload to the backend
   const handleCheckFile = async(e: React.MouseEvent<HTMLButtonElement>) : Promise<void> => {
     e.preventDefault();
-    const formdata = new FormData();
-    uploadedImages.forEach((image) => {
-      formdata.append("image", image.img);
-    });
-    console.log(formdata.getAll("image"));
-    const response = await axios.post("")
+    try{
+        const formdata = new FormData();
+        uploadedImages.forEach((image) => {
+          formdata.append("image", image.file);
+        });
+        console.log(formdata.getAll("image"));
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/lm/detect`, formdata, {
+          withCredentials: true
+        });
+        console.log(response.data[0].class);
+        console.log(response.data);
+        setResult(response.data[0].class);
+    }catch(error){
+      console.log(error);
+    }
   }
+
 
   return (
     <div className="flex flex-row justify-center align-center">
       <div className="flex flex-col mt-[10%]">
-        <div className="flex flex-row space-x-4">
-          {uploadedImages.map((img) => (
-            <div key={img.id}>
-              <img 
-              src={img.img} 
-              alt="an uploaded image" 
-              className="max-w-[200px] max-h-[200px]"
-              />
-            </div>
-          ))}
+        <div className="flex flex-col space-x-4 space-y-5">
+          <div className="text-center">
+            <p className="text-red-500">
+              Please note that clicking on an image will delete it!
+            </p>
+          </div>
+          <div className="flex flex-row mb-5">
+            {uploadedImages.map((img) => (
+              <div key={img.id}>
+                <img 
+                src={img.img} 
+                alt="an uploaded image" 
+                className="max-w-[200px] max-h-[200px]"
+                onClick={() => removeSelectedImg(img.id)}
+                />
+              </div>
+            ))}
+          </div>
         </div>
         <div
         className="flex flex-row space-x-4 p-2"
@@ -77,6 +103,7 @@ export default function Home() {
             ref={uploadElement}
             multiple
             onChange={handleFileUpload}
+            name="image"
             />
             <button
             className="p-1 bg-blue-300 cursor-pointer"
@@ -85,6 +112,11 @@ export default function Home() {
               Detect Image
             </button>
           </form>
+        </div>
+        <div
+        className="mt-[5%]"
+        >
+           <p>Image detected as: {result}</p>
         </div>
       </div>
     </div>
